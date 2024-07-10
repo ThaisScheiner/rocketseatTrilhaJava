7a99ebb3f28612293d34e2b1952c697f099d8203
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2024-07-10 03:15:37 -0300

    commit aula 1

---
+++ src/main/java/com/rocketseat/planner/participant/ParticipantService.java
@@ -0,0 +1,27 @@
+package com.rocketseat.planner.participant;
+
+import com.rocketseat.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+
+    @Autowired
+    private ParticipantRepository repository;
+
+    public void registerParticipantsToEvent(List<String> participantsToInvite, Trip trip) {
+        List<Participant> participants = participantsToInvite.stream().map(email -> new Participant(email, trip)).toList();
+        this.repository.saveAll(participants);
+
+        System.out.println(participants.get(0).getId());
+    }
+
+    public void triggerConfirmationEmailToParticipants(UUID tripId){};
+
+    public void triggerConfirmationEmailToParticipant(String email){};
+
+}
---
+++ src/main/java/com/rocketseat/planner/trip/TripController.java
@@ -0,0 +1,39 @@
+package com.rocketseat.planner.trip;
+
+import com.rocketseat.planner.participant.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/trips")
+public class TripController {
+
+    @Autowired
+    private TripRepository repository;
+
+    @Autowired
+    private ParticipantService participantService;
+    //Trip
+
+    @PostMapping
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
+        Trip newTrip = new Trip(payload);
+
+        this.repository.save(newTrip);
+        this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip);
+
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+        Optional<Trip> trip = this.repository.findById(id);
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+}
